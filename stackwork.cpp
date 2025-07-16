86994d1e9e7c31c6e6532b6bce75558043049d58
Yiming <2018565194@qq.com>
2025-07-16 15:22:45 +0800

    Update stackwork.cpp

--- stackwork.cpp
+++ stackwork.cpp
@@ -1,40 +1,40 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<vector>
-//层序遍历
-using namespace std;
-class Solution {
-public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-
-        queue<TreeNode*> q;
-        int levesize = 0;//每一层的个数
-        if (root)
-        {
-            q.push(root);
-            levesize = 1;
-        }
-        vector<vector<int>> vv;//储存每一层的数据
-        while (!q.empty())
-        {
-            vector<int> v;
-            while (levesize--)
-            {
-                TreeNode* front = q.front();
-                q.pop();//取出根节点，放入子节点
-                v.push_back(front->val);
-                if (front->left)
-                {
-                    q.push(front->left);
-                }
-                if (front->right)
-                {
-                    q.push(front->right);
-                }
-
-            }
-            vv.push_back(v);
-            levesize = q.size();//下一层的数据个数
-        }
-        return vv;
-    }
-};
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<vector>
+////层序遍历
+//using namespace std;
+//class Solution {
+//public:
+//    vector<vector<int>> levelOrder(TreeNode* root) {
+//
+//        queue<TreeNode*> q;
+//        int levesize = 0;//每一层的个数
+//        if (root)
+//        {
+//            q.push(root);
+//            levesize = 1;
+//        }
+//        vector<vector<int>> vv;//储存每一层的数据
+//        while (!q.empty())
+//        {
+//            vector<int> v;
+//            while (levesize--)
+//            {
+//                TreeNode* front = q.front();
+//                q.pop();//取出根节点，放入子节点
+//                v.push_back(front->val);
+//                if (front->left)
+//                {
+//                    q.push(front->left);
+//                }
+//                if (front->right)
+//                {
+//                    q.push(front->right);
+//                }
+//
+//            }
+//            vv.push_back(v);
+//            levesize = q.size();//下一层的数据个数
+//        }
+//        return vv;
+//    }
+//};
