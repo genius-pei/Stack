f03431bbf6cb11562832ee990a79ec2a6307f7dd
Yiming <2018565194@qq.com>
2025-07-16 20:05:06 +0800

    Create satck_test.cpp

---
+++ satck_test.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"Stack.h"
+#include<stack>
+#include<queue>
+using namespace std;
+int main()
+{
+	yiming::stack<int,vector<int>> st;
+	st.push(10);
+	st.push(12);
+	st.push(15);
+	st.push(11);
+	cout << st.top() << " " << endl;
+	while (!st.empty())
+	{
+		cout << st.top() << " ";
+		st.pop();
+
+	}
+	return 0;
+}
