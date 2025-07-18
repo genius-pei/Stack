52555de6db77c062b0d1e6717decedef293a219a
Yiming <2018565194@qq.com>
2025-07-18 15:37:27 +0800

    Update testcpp.cpp

--- testcpp.cpp
+++ testcpp.cpp
@@ -3,28 +3,43 @@
 #include<stack>
 #include<queue>
 using namespace std;
+//int main()
+//{
+//	stack<int> st;
+//	st.push(1);
+//	st.push(3);
+//	st.push(4);
+//	while (!st.empty())
+//	{
+//		cout << st.top() << " ";
+//		st.pop();
+//	}
+//	cout << endl;
+//	queue<int>q;
+//
+//	q.push(1);
+//	q.push(2);
+//	q.push(3);
+//	q.push(4);
+//	while (!q.empty())
+//	{
+//		cout << q.front()<<" ";
+//		q.pop();
+//	}
+//	return 0;
+//}
 int main()
 {
-	stack<int> st;
-	st.push(1);
-	st.push(3);
-	st.push(4);
-	while (!st.empty())
+	priority_queue<int> pq;
+	pq.push(4);
+	pq.push(2);
+	pq.push(5);
+	pq.push(6);
+	while (!pq.empty())
 	{
-		cout << st.top() << " ";
-		st.pop();
+		cout << pq.top() << " ";
+		pq.pop();
 	}
 	cout << endl;
-	queue<int>q;
-
-	q.push(1);
-	q.push(2);
-	q.push(3);
-	q.push(4);
-	while (!q.empty())
-	{
-		cout << q.front()<<" ";
-		q.pop();
-	}
 	return 0;
 }
