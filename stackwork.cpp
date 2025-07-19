a438993de771f0a984edb84543fec21396e136bb
Yiming <2018565194@qq.com>
2025-07-19 22:10:41 +0800

    Update stackwork.cpp

--- stackwork.cpp
+++ stackwork.cpp
@@ -38,3 +38,16 @@
 //        return vv;
 //    }
 //};
+
+//class Solution {
+//public:
+//    int findKthLargest(vector<int>& nums, int k) {
+//
+//        priority_queue<int> pq(nums.begin(), nums.end());
+//        for (int i = 1;i < k;i++)
+//        {
+//            pq.pop();
+//        }
+//        return pq.top();
+//    }
+//};
